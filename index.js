7e0d33582e2010179344678ed63908fa688e1ddc
Jia <zhangjia@mit.edu>
2014-11-23 14:30:51 -0500

    working line drawing of subways

--- index.js
+++ index.js
@@ -1,85 +1,69 @@
-console.log("file name:" + csv)
-function dataDidLoad(error, data) {
-	var data = data
-	drawLineGraph(data)
-}
-
 $(function() {
 	// Window has loaded
 	queue()
-		.defer(d3.csv, csv)
+		.defer(d3.json, "static/geojson/lines.geojson")
+		.defer(d3.json, "static/geojson/stations.geojson")
 		.await(dataDidLoad);
 })
-var redlineLabels = ["alewife","davis","porter","harvard","central","kendall","charles mgh","park street","downtown crossing","south station","broadway","andrew","jfk umass","north quincy","wollaston","quincy center","quincy adams","braintree"]
 
-function drawLineGraph(data){
-    data.forEach(function(d) {
-        d.station = d.station
-        d.mean= +parseInt(d.mean);
-    });
-	var margin = {top: 30, right: 20, bottom: 130, left: 50},
-	    width = 600 - margin.left - margin.right,
-	    height = 500 - margin.top - margin.bottom;
-	// Set the ranges
-	var x = d3.scale.linear().range([0, width]);
-	var y = d3.scale.linear().range([height, 0]);
-
-	// Define the axes
-	var xAxis = d3.svg.axis().scale(x)
-	    .orient("bottom").ticks(15)
-		.tickFormat(formatStop);;
-
-	var yAxis = d3.svg.axis().scale(y)
-	    .orient("left").ticks(16)
-		.tickFormat(formatCurrency);
-	// Define the line
-	var valueline = d3.svg.line()
-	    .x(function(d,i) { return x(i); return x(d.station); })
-	    .y(function(d) { return y(parseInt(d.mean)); });
-	// Adds the svg canvas
-	var svg = d3.select("#containers")
-		.append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", 
-		  "translate(" + margin.left + "," + margin.top + ")");
-
-    // Scale the range of the data
-    x.domain(d3.extent(data, function(d,i) { return i; }));
-   // y.domain([0, d3.max(data, function(d) { return parseInt(d.mean); })]);
-   	y.domain([0,150000])
-    // Add the valueline path.
-    svg.append("path")
-        .attr("class", "line")
-        .attr("d", valueline(data))
-		.attr("fill","none")
-	   .attr("stroke", "#dd2222")
-   		.attr("stroke-width",2);
-
-    // Add the X Axis
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-   		.selectAll("text")
-       .attr("y", 0)
-       .attr("x", 9)
-       .attr("dy", ".35em")
-       .attr("transform", "rotate(90)")
-       .style("text-anchor", "start");
-    // Add the Y Axis
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
+function dataDidLoad(error, line, stops) {
+	//console.log(line)	
+	//console.log(stops)
+	drawLineGraph(line,stops)
 }
-function formatStop(d,i) {
-	return capitaliseFirstLetter(redlineLabels[i])
-}
-function formatCurrency(d) {
-	return "$" + d;
-}
-function capitaliseFirstLetter(string)
-{
-    return string.charAt(0).toUpperCase() + string.slice(1);
+
+
+
+function drawLineGraph(lines,stops){
+	//console.log(stops)
+	var width = 600;
+	var height = 620;
+	var svg = d3.select("#subway")
+			.append("svg")
+			.attr("width",width)
+			.attr("height",height);
+	
+	var projection = d3.geo.mercator()
+		.scale([90000])
+		.center([ -71.11,42.319771])
+		.translate([width/2,height/2])
+		
+	var path = d3.geo.path()
+		.projection(projection)
+		
+	var tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d;});
+		
+	svg.selectAll("path")
+        .data(lines.features)
+        .enter()
+        .append("path")
+		.attr("d",path)
+		.attr("opacity", 0.5)
+		.style("fill", "none")
+        .style("stroke-width", "2")
+        .style("stroke", function(d){
+			//console.log(d.properties.LINE);
+			return d.properties.LINE
+		})
+			
+	svg.selectAll("circle")
+		.data(stops.features)
+		.enter()
+		.append("circle")
+		.attr("cx", function(d){
+			return projection(d.geometry.coordinates)[0]
+		})
+		.attr("cy", function(d){
+			console.log(d.properties.LINE,",",d.properties.STATION,",",d.geometry.coordinates[1],",",d.geometry.coordinates[0])
+			return projection(d.geometry.coordinates)[1]
+		})
+		.attr("r", 4)
+        .style("fill", function(d){
+			return d.properties.LINE
+		})
+		.style("stroke","white")
+		.attr("opacity", 0.5)
+		.on("mouseover", function(d){
+			d3.select("#chart-title").html(d.properties.STATION)
+		})
 }
