16529e3a5ec955ea2a54b9346bdf7b69f6301ede
Jia <zhangjia@mit.edu>
2015-01-19 10:24:34 -0500

    dc update, working on interaction, uploaded this version to site

--- index_2.js
+++ index_2.js
@@ -15,20 +15,53 @@ $(function() {
 function dataDidLoad(error,stations,data,blocks) {
 	//console.log(line)	
 	//console.log(stops)
-	drawSubwayStops(stations,data,blocks)
-	drawAllBlocks(blocks)
-
+	var svg = d3.select("#subway")
+		.append("svg")
+		.attr("width",mapWidth)
+		.attr("height",mapHeight);
+	formatSubwayStopsByLine(stations,data,blocks,svg)
+	drawAllBlocks(blocks,svg)
+	
 //	var lineColor = "red"
 //	drawLineGraph(lineColor,stops_steve)
 }
-function drawAllBlocks(blocks){
-	var width = 500;
-	var height = 500;
+function drawAllBlocks(blocks,svg){
 	var projection = d3.geo.mercator()
 		.scale(scale)
 		.center(center)
-		.translate([width/2,height/2])
+		.translate(translate)
 	//	console.log(currentBlocks)
+	var path = d3.geo.path()
+		.projection(projection)
+	var svg = svg
+	svg.selectAll("path")
+        .data(blocks.features)
+        .enter()
+        .append("path")
+		.attr("class","blockBoundaries")
+		.attr("d",path)
+		.attr("stroke", "#aaa")
+		.style("fill", "none")
+	    .style("stroke-width", .5)
+	    .style("opacity",.5)
+}
+
+function drawBlocks(blocks,currentCoordinates,data){
+	var stationName = currentCoordinates[0]
+	var currentData = data.stations[stationName]["blockgroups"]
+	var currentBlocks = []
+	//console.log(blocks)
+	d3.selectAll("#subway .blockBoundaries").remove()
+	for(var i in currentData){
+		currentBlock = currentData[i][0]
+		currentBlocks.push(currentBlock)
+	}
+	var projection = d3.geo.mercator()
+		.scale(scale)
+		.center(center)
+		.translate(translate)
+	//	console.log(currentBlocks)
+		
 	var path = d3.geo.path()
 		.projection(projection)
 	var svg = d3.select("#subway svg")
@@ -41,7 +74,14 @@ function drawAllBlocks(blocks){
 		.attr("stroke", "#000")
 		.style("fill", "none")
 	    .style("stroke-width", .5)
-	    .style("opacity",1)
+	    .style("opacity", function(d){
+			//console.log(d.properties.LINE);
+			if(currentBlocks.indexOf(d["GEO_ID"])>-1){
+				return .5
+			}else{
+				return .1			
+			}
+		})
 }
 function calculateAverageIncome(blockgroups){
 	//console.log(blockgroups.length)
@@ -63,7 +103,8 @@ function calculateAverageIncome(blockgroups){
 	//console.log([parseInt(sum),populationTotal,sum/populationTotal])
 	return [blockgroups.length,populationTotal,sum/populationTotal]
 }
-
+function calculateLineWideAverage(){}
+function calculateSystemWideAverage(){}
 function calculateDistance(origin,coordinates){
 	//console.log(origin)
 	//console.log(coordinates)
@@ -74,15 +115,17 @@ function calculateDistance(origin,coordinates){
 	return distance
 }
 function formatLineData(lineColor,data){
-	var currentData = data.lines[lineColor]
+	var currentData = data.lines[lineColor]["stations"]
 	//console.log(lineColor)
-	//console.log(currentData)
+	var orderedStations = data.lines[lineColor]["primary_routes"][0]
+	console.log([lineColor,orderedStations])
 	var stationList = []
 	var cummulativeDistance = 0
 	//for each station, get groups and average data
-	for(var station in currentData){
-		var currentStation = currentData[station][0]
-		var originStation = currentData[0][0]
+	for(var station in orderedStations){
+		var currentStation = orderedStations[station]
+		console.log(currentStation)
+		var originStation = orderedStations[0]
 		//console.log([originStation,currentStation])
 		var blockgroups = data.stations[currentStation]["blockgroups"]
 		var coordinates = data.stations[currentStation]["coordinates"]
@@ -95,6 +138,7 @@ function formatLineData(lineColor,data){
 		stationList.push([currentStation,income,cummulativeDistance])
 		
 	}
+	console.log(stationList)
 	return stationList
 }
 function drawLineGraph(lineColor,data){
@@ -173,93 +217,29 @@ function displayDataByStation(station,data){
 	var averageIncome = calculateAverageIncome(blockgroups)
 	return "average median income: "+parseInt(averageIncome[2])+"<br/>population: "+averageIncome[1]+"<br/>blocks: "+averageIncome[0]
 }
-function drawBlocks(blocks,station,data){
-	var currentData = data.stations[station]["blockgroups"]
-	var currentBlocks = []
-	d3.selectAll("#subway .blockBoundaries").remove()
-	for(var i in currentData){
-		currentBlock = currentData[i][0]
-		currentBlocks.push(currentBlock)
-	}
-	var width = 500;
-	var height = 500;
+function drawSubwayStops(blocks,currentCoordinates,data,svg,fill,radius){
 	var projection = d3.geo.mercator()
 		.scale(scale)
 		.center(center)
-		.translate([width/2,height/2])
-	//	console.log(currentBlocks)
-		
-	var path = d3.geo.path()
-		.projection(projection)
-	var svg = d3.select("#subway svg")
-	svg.selectAll("path")
-        .data(blocks.features)
-        .enter()
-        .append("path")
-		.attr("class","blockBoundaries")
-		.attr("d",path)
-		.attr("stroke", "#000")
-		.style("fill", "none")
-	    .style("stroke-width", 1)
-	    .style("opacity", function(d){
-			//console.log(d.properties.LINE);
-			if(currentBlocks.indexOf(d["GEO_ID"])>-1){
-				return 1
-			}else{
-				return .5			
-			}
-		})
-}
-function drawSubwayStops(stops,data,blocks){
-	var width = 500;
-	var height = 500;
-	var svg = d3.select("#subway")
-			.append("svg")
-			.attr("width",width)
-			.attr("height",height);
-	var projection = d3.geo.mercator()
-		.scale(scale)
-		.center(center)
-		.translate([width/2,height/2])
-	var stationData = data.stations
-	console.log(stationData)	
-			for
-	svg.selectAll("circle")
-		.data(stationData )
-		.enter()
-		.append("circle")
-		.attr("cx", function(d,i){
-			console.log(d)
-			return projection(d.properties.coordinates)[0]
-		})
-		.attr("cy", function(d){
-			return projection(d.properties.coordinates)[1]
-		})
-		.attr("r", 3)
-        .style("fill", function(d){
-			return "#000"
-			return d.properties.LINE
-		})
-		.attr("class", function(d){
-			return d.properties.LINE
-		})
-		.style("stroke","white")
-		.attr("opacity", .5)
+		.translate(translate)
+
+	svg.append("circle")
+		.attr("cx", projection(currentCoordinates[1])[0]+radius)
+		.attr("cy", projection(currentCoordinates[1])[1]+radius)
+		.attr("r",3)
+	    .style("fill",fill)
+		.attr("opacity", 1)
+		.attr("stroke","#fff")
 		.on("mouseover", function(d){
-			d3.select(this).style("fill", "#000000")
-			d3.selectAll("."+d.properties.LINE).attr("opacity",1)
-			var station = d.properties.STATION			
-			drawBlocks(blocks,station,data)
-			var stationData = displayDataByStation(station,data)
-			d3.select("#station_rollover").html("stop:"+d.properties.STATION+"</br> line:"+d.properties.LINE+"</br>"+stationData)			
+			var station = data[0]	
+			drawBlocks(blocks,currentCoordinates,data)
+		//	var stationData = displayDataByStation(station,data)
+			//d3.select("#station_rollover").html("stop:"+d.properties.STATION+"</br> line:"+d.properties.LINE+"</br>"+stationData)			
 		})
 		.on("mouseout",function(d){
-			d3.select(this).style("fill", function(d){
-				return d.properties.LINE
-			})
-			d3.selectAll("."+d.properties.LINE).attr("opacity",.2)
-			d3.selectAll("."+clicked).attr("opacity",1)
-			d3.selectAll("#subway .blockBoundaries").attr("opacity",.1)
+//			d3.selectAll("."+d.properties.LINE).attr("opacity",.2)
+//			d3.selectAll("."+clicked).attr("opacity",1)
+//			d3.selectAll("#subway .blockBoundaries").attr("opacity",.1)
 		})
 		.on("click",function(d){
 			clicked = d.properties.LINE
@@ -273,53 +253,102 @@ function drawSubwayStops(stops,data,blocks){
 			d3.selectAll("."+d.properties.LINE).attr("opacity",1)
 		})
 }
-function drawSubwayLines(stops,data,blocks){
-	var width = 500;
-	var height = 500;
-	var svg = d3.select("#subway")
-			.attr("width",width)
-			.attr("height",height);
-	
+function buildStationLineDictionary(data){
+	//console.log(data.lines)
+	var stationLineDictionary = {}
+	for(var line in data.lines){
+		var currentLine = data.lines[line]
+		//console.log(currentLine.stations)
+		for(var station in currentLine.stations){
+			var currentStation = currentLine.stations[station]
+			var stationName = currentStation[0]
+			var stationRoutes = currentStation[1]["ROUTE"]
+			stationLineDictionary[stationName]=stationRoutes
+		}
+	}
+	//console.log(stationLineDictionary)
+	return stationLineDictionary
+}
+function formatSubwayStopsByLine(stops,data,blocks,svg){
+	var stationLineDictionary = buildStationLineDictionary(data)
 	var projection = d3.geo.mercator()
 		.scale(scale)
 		.center(center)
-		.translate([width/2,height/2])
+		.translate(translate)
+	var stationData = data.lines
+	var stationLocationData = data.stations
+	var index = 0
+	//console.log(stationLocationData)
+	for(var route in stationData){
+		index +=1
+		var offset = index
+		var currentRoute = stationData[route]["primary_routes"][0]
+		var color = route
+		drawSubwayLines(currentRoute,data,svg,color,offset)
+
+		for(var station in currentRoute){
+			var currentStation = currentRoute[station]
+			var currentStationCoordinates = [currentStation,data.stations[currentStation].coordinates]
+			//console.log(data.lines)
+			var routes = stationLineDictionary[currentStation].split(",")
+			for(var route in routes){
+				var fill = colorDictionary[routes[route].replace(/\s+/g, '')]
+				var radius = route*6
+				//console.log([radius,fill])
+				drawSubwayStops(blocks,currentStationCoordinates,data,svg,fill,radius)
+			}
+		}
+	}
+	
+}
+function drawSubwayLines(route,data,svg,color,offset){
+//	console.log(route)
+//	console.log(data)
+	var routeLine = []
+	for(var station in route){
+		var currentStation = route[station]
+		var coordinates = data.stations[currentStation].coordinates
+		//console.log(coordinates)
+		routeLine.push(coordinates)
+	}
+	var projection = d3.geo.mercator()
+		.scale(scale)
+		.center(center)
+		.translate(translate)
 		
-	var path = d3.geo.path()
-		.projection(projection)
-	//var tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d;});
-//	var clicked = null;
-	//svg.selectAll("path")
-    //    .data(stops.features)
-    //    .enter()
-    //    .append("path")
-	//	.attr("class", function(d){
-	//		return d.properties.LINE
-	//   	})
-	//	.attr("class","path")
-	//	.attr("d",path)
-	//	.attr("opacity", .2)
-	//	.style("fill", "none")
-    //    .style("stroke-width", 1)
-    //    .style("stroke", function(d){
-	//		//console.log(d.properties.LINE);
-	//		return "#aaa"
-	//		return d.properties.LINE
-	//	})
-	//	.on("mouseover", function(d){
-	//		d3.select("#chart-title").html(d.properties.STATION)
-	//		d3.selectAll("."+d.properties.LINE).attr("opacity",1)
-	//	})
+var line = d3.svg.line()
+    .x(function(d) { 
+		return parseInt(projection(d)[0])+offset; 
+	})
+    .y(function(d) { 
+		//console.log(["y",projection(d)[1]]);
+		return parseInt(projection(d)[1])+offset; 
+	});
+		//var tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d;});
+	//	var clicked = null;
+	svg.selectAll("path")
+        .data(routeLine)
+        .enter()
+        .append("path")
+	.attr("class",color)
+		.attr("d",line(routeLine))
+		.attr("fill","none")
+		.style("stroke-width", 5)
+		.style("stroke",colorDictionary[color])
+	.on("mouseover", function(d){
+ 		d3.select("#chart-title").html(color)
+ 		d3.selectAll("."+d.properties.LINE).attr("opacity",1)
+	})
 	//	.on("mouseout",function(d){
 	//		d3.selectAll("."+d.properties.LINE).attr("opacity",.2)
 	//		d3.selectAll("."+clicked).attr("opacity",1)
 	//	})
-	//	.on("click",function(d){
-	//		clicked = d.properties.LINE
-	//		var lineColor = d.properties.LINE
-	//		d3.select("#chart-title").html(d.properties.LINE)
-	//		drawLineGraph(lineColor,data)
-	//		d3.selectAll("path").attr("opacity",0.2)
-	//		d3.selectAll("."+d.properties.LINE).attr("opacity",1)
-	//	})
+		.on("click",function(d){
+//			clicked = d.properties.LINE
+			var lineColor = color
+//			d3.select("#chart-title").html(d.properties.LINE)
+			drawLineGraph(lineColor,data)
+			d3.selectAll("path").attr("opacity",0.2)
+			d3.selectAll("."+d.properties.LINE).attr("opacity",1)
+		})
 }
